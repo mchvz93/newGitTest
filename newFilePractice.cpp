0c0921c1cea2b793e303a28e36573e8e111e7585
mchvz93 <mchavez6667@gmail.com>
2019-09-12 12:25:32 -0700

    added newFilePractice.cpp/ newFilePractice.hpp

---
+++ newFilePractice.cpp
@@ -0,0 +1,10 @@
+//
+//  newFilePractice.cpp
+//  
+//
+//  Created by XxTheEyeOfSauronxX on 9/12/19.
+//
+
+#include "newFilePractice.hpp"
+
+std::cout << "this is just a test" << std::endl;
---
+++ newFilePractice.hpp
@@ -0,0 +1,13 @@
+//
+//  newFilePractice.hpp
+//  
+//
+//  Created by XxTheEyeOfSauronxX on 9/12/19.
+//
+
+#ifndef newFilePractice_hpp
+#define newFilePractice_hpp
+
+#include <stdio.h>
+
+#endif /* newFilePractice_hpp */
